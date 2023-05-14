264d454b244ff6791aaa48af6eb135cf566a85f2
Scardigno1982 <sergioscardigno82@gmail.com>
2023-05-14 19:06:36 -0300

    Agrego Node.js y levanto una web con next.js
    ihu

---
+++ html/hosting-laplata.com.ar/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+<title>Hosting La Plata </title>
+
+</head>
+<body>
+    <h1>Bienvenido a Hosting La Plata COM.AR</h1>
+    <p>Esta es una p�gina de prueba.</p>
+<?php 
+echo "Hola Mundo PHP";
+
+echo "<br>";
+
+phpinfo();
+
+$x = 10;
+$c = 15;
+
+$d = $x+$c;
+
+echo $d;
+
+?>
+
+</body>
+</html>
